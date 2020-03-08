339cb0747503f4e5b4d7f297f595446cda7b0737
Nickleus Jimenez <nickleus.jimenez@gmail.com>
2020-03-08 21:36:38 +0800

    Logic for HTML dropdown list

--- Lazydog.Model/Service/ExcuseMsgTemplateService.cs
+++ Lazydog.Model/Service/ExcuseMsgTemplateService.cs
@@ -14,7 +14,7 @@ namespace Lazydog.Model.Service
             |FarewellWord ~, <br/>
             `Name";
         }
-        private string ReplaceVariablesToHtmlTextBox(string regExPattern,string input)
+        private string ReplaceVariablesToHtmlTextBox(string regExPattern, string input)
         {
             string templateString = input;
             Regex regexElements = new Regex(regExPattern, RegexOptions.IgnoreCase);
@@ -26,10 +26,56 @@ namespace Lazydog.Model.Service
             }
             return templateString;
         }
-        public string GenerateHtmlOfTemplate(string templateString)
+        private string createSelectTagfromTemplate(string substringToReplace, IList<string> optionContent)
         {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("<select name='{0}'>", substringToReplace.Substring(1));
+            foreach (string option in optionContent)
+            {
+                sb.AppendFormat("<option>{0}</option>", option);
+            }
+            sb.Append("</select>");
+            return sb.ToString();
+        }
+        private string ReplaceVariablesToHtmlDdl(string substringToReplace, string input, IList<string> optionContent = null)
+        {
+            if (optionContent == null)
+            {
+                optionContent = new List<string>();
+                optionContent.Add("Thanks");
+                optionContent.Add("Sincerly");
+            }
+            return input.Replace(substringToReplace, createSelectTagfromTemplate(substringToReplace, optionContent));
+        }
+        private string ReplaceWordsForHtmlDll(string templateString, string regExPattern, Dictionary<string, IList<string>> OptionsList)
+        {
+            Regex regexElements = new Regex(regExPattern, RegexOptions.IgnoreCase);
+            string result = templateString;
+            foreach (Match m in regexElements.Matches(templateString))
+            {
+                string ddlName = m.Value.Substring(1);
+                IList<string> options;
+                if (OptionsList.ContainsKey(ddlName))
+                {
+                    options = OptionsList[ddlName];
+                }
+                else
+                {
+                    options = null;
+                }
+                result = ReplaceVariablesToHtmlDdl(m.Value, result, options);
+            }
+            return result;
+        }
+        public string GenerateHtmlOfTemplate(string templateString,  Dictionary<string, IList<string>> OptionsList = null)
+        {
+            if (OptionsList == null)
+            {
+                OptionsList = new Dictionary<string, IList<string>>();
+            }
+            templateString = ReplaceWordsForHtmlDll(templateString, @"[|]\w+", OptionsList);//ReplaceVariablesToHtmlDdl(substringToReplace, templateString, optionContent);
             return ReplaceVariablesToHtmlTextBox(@"[`]\w+", templateString).Replace(" ~", String.Empty);
         }
-        
+
     }
 }
--- Lazydog.model.Service.Test/ExcuseMsgTemplateServiceTest.cs
+++ Lazydog.model.Service.Test/ExcuseMsgTemplateServiceTest.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Lazydog.Model.Service;
+using System.Collections.Generic;
+
 namespace Lazydog.model.Service.Test
 {
     [TestClass]
@@ -16,7 +18,7 @@ namespace Lazydog.model.Service.Test
             ExcuseMsgTemplateService msgService = new ExcuseMsgTemplateService();
             string result = msgService.GenerateHtmlOfTemplate(sampleTemplate);
             Assert.IsFalse(result.Contains('`'));
-            Assert.IsTrue(result.Contains('<'));
+            Assert.IsTrue(result.Contains("<input"));
         }
         [TestMethod]
         public void Test_RemoveTilde()
@@ -28,6 +30,35 @@ namespace Lazydog.model.Service.Test
             ExcuseMsgTemplateService msgService = new ExcuseMsgTemplateService();
             string result = msgService.GenerateHtmlOfTemplate(sampleTemplate);
             Assert.IsFalse(result.Contains('~'));
+        }
+        [TestMethod]
+        public void Test_RemoveForDropDownLists()
+        {
+            string sampleTemplate = @"Hi, <br/>
+            I `Name ~. wish to be excused for I have `Excuse ~. <br/>
+            |FarewellWord ~, <br/>
+            `Name";
+            ExcuseMsgTemplateService msgService = new ExcuseMsgTemplateService();
+            string result = msgService.GenerateHtmlOfTemplate(sampleTemplate);
+            Assert.IsTrue(result.Contains("<select"));
+        }
+        [TestMethod]
+        public void Test_RemoveForMultipleDropDownLists()
+        {
+            string sampleTemplate = @"Hi |sirmam ~, <br/>
+            I `Name ~. wish to be excused for I have `Excuse ~. <br/>
+            |FarewellWord ~, <br/>
+            `Name";
+            var OptionsList = new Dictionary<string, IList<string>>();
+            IList<string> sirMamOptions = new List<string>();
+            ExcuseMsgTemplateService msgService = new ExcuseMsgTemplateService();
+
+            sirMamOptions.Add("sir");
+            sirMamOptions.Add("mam");
+            OptionsList.Add("sirmam", sirMamOptions);
+            string result = msgService.GenerateHtmlOfTemplate(sampleTemplate,OptionsList);
+            Assert.IsTrue(result.Contains(">sir"));
+            Assert.IsTrue(result.Contains("mam"));
         }
     }
 }
