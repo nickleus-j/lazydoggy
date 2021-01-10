9e47c5f62ffc40326d214fdd30587293d97f3ac1
Nickleus Jimenez <nickleus.jimenez@gmail.com>
2021-01-10 19:12:35 +0800

    Animation page

--- DaLazyDog/Controllers/HomeController.cs
+++ DaLazyDog/Controllers/HomeController.cs
@@ -79,5 +79,9 @@ namespace DaLazyDog.Controllers
         {
             return View();
         }
+        public IActionResult Animations()
+        {
+            return View();
+        }
     }
 }
