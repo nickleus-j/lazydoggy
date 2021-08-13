62205a8461c94b6ff80b603ff1a3c885f25d52c3
Nickleus-J <nickleus.jimenez@gmail.com>
2021-08-13 10:13:11 -0700

    Current Changes for Styling of the app

--- DaLazyDog/Resources/Views.Excuse.Index.Designer.cs
+++ DaLazyDog/Resources/Views.Excuse.Index.Designer.cs
@@ -19,7 +19,7 @@ namespace DaLazyDog.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Views_Excuse_Index {
@@ -106,7 +106,7 @@ namespace DaLazyDog.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to We have prepared excuses for you.
+        ///   Looks up a localized string similar to We have prepared excuses for you. Just Click.
         /// </summary>
         public static string Prepared_Excuses {
             get {
